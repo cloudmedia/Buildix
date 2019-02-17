4db97621e861720333e5aa03d6b1f661229b79bc
Jay Simons <jay@cloudulus.net>
2019-02-17 14:55:18 -0600

    Initial commit

---
+++ platforms/browser/www/js/nav.js
@@ -0,0 +1,25 @@
+var menu;
+$(document).touch(function(e){
+    var clickedClass = $(e.target).attr('class');
+    if (clickedClass != 'cat-btn' && $("#menu").hasClass('active')) menu.toggle();
+});
+
+$(document).ready(function () {
+    menu = new menuSlider('menu');
+    menu.setAnimClass('bounceInRight');
+});
+
+function initNav()
+{
+    $(".cat-btn").unbind().touch(function(){
+        var id = $(this).data('id');
+        $(".cat-cont").hide();
+        $("#"+id).fadeIn();
+    });
+
+    $(".zone-btn").unbind().touch(function(){
+        scrollTop();
+        loadingOn();
+        $("#main").load("/zone?zid="+$(this).data('zid'));
+    });
+}
