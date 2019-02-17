a5bee4c42af50bad76d2760a1f4a6d64395d4ffa
Jay Simons <jay@cloudulus.net>
2019-02-17 15:26:15 -0600

    Updated config.xml, icons

---
+++ platforms/windows/www/js/index.js
@@ -0,0 +1,209 @@
+﻿var server = null;
+var login = false;
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        server = localStorage.getItem('server');
+
+        initMain();
+
+        if (server)      
+        {
+            $("#main-h1").text("Connecting...");
+            console.log('Received Event: ' + id);
+            console.log('Connecting to '+server+'...');
+            $.ajax({
+                url: server+'/login?action=get-status',
+                method: 'get',
+                timeout: 5000,
+                dataType: 'json',
+                success: processStatus,
+                error: errorStatus
+            });
+        }else{
+
+        }
+    }
+};
+
+app.initialize();
+
+function processStatus(data)
+{
+    console.log(data);
+    if (data.status == 1)
+    {
+        login = true;
+        doLogin();
+    }else{
+        $("#main").load('html/login.html');
+    }
+}
+
+function errorStatus()
+{
+    notify2("Failed to connect to "+server+"!");
+}
+
+function initMain()
+{
+    loadingOff();
+
+    $("#main-connect-btn").unbind().touch(function(){
+        var newServer = $("#main-server").val();
+        var valid = new Valid(newServer);
+        if (valid.isHostname())
+        {
+            server = 'https://'+newServer;
+            localStorage.setItem('server', server);
+            window.location.replace("index.html");
+        }else{
+            alert(valid.message);
+        }
+    });
+
+    $(".btnLoad").unbind().touch(function(){
+        scrollTop();
+        loadingOn();
+        var url = $(this).data('url');
+        switch ($(this).data('target'))
+        {
+            case "main":
+                $("#main").load(server+url);
+            break;
+            case "window":
+                window.location.replace(url);
+            break;
+            case "blank":
+                window.open(url);
+            break;
+        }
+    });
+
+    $(".logout-btn").unbind().touch(function(){
+        askLogout();
+    });
+
+    $(".clear-btn").unbind().touch(function(){
+        server = null;
+        localStorage.setItem('server', null);
+        window.location.replace("index.html");
+    });
+
+    $(".reset-btn").unbind().touch(function(){
+        window.location.replace("index.html");
+    });
+
+    if (login) initNav();
+}
+
+function loadingOn()
+{
+    $("#site-logo-svg").attr('src', 'images/bx-loading.svg').removeClass().addClass('animated infinite pulse');
+}
+
+function loadingOff()
+{
+    $("#site-logo-svg").attr('src', 'images/bx-logo.svg').removeClass().addClass('animated rubberBand');
+}
+
+function askLogout()
+{
+    var n = new Notify2("Are you sure you want to log out?", "info");
+    n.doConfirm('doLogout()');
+    n.notify();
+}
+
+function doLogin()
+{
+    loadingOn();
+    $("#main").load('html/dashboard.html');
+    setTimeout(function(){
+        $("#nav-vp").load(server+"/api/get-nav");
+    },1000);
+}
+
+function doLogout()
+{
+    scrollTop();
+    $.getJSON(server+'/logout?action=app', function(data){
+        if (data.status == 1)
+        {
+            loadingOn();
+            $("#nav-vp").empty();
+            $("#main").load('html/login.html');
+        }else{
+            notify2(data.message);
+        }
+    });
+}
+
+$.fn.touch = function(callback)
+{
+    if (typeof callback == 'function')
+    {
+        $(this).on('click touch', callback);
+    }
+}
+
+$.fn.ok = function()
+{
+    $(this).css('background-color', 'green');
+}
+
+$.fn.bad = function()
+{
+    $(this).css('background-color', '#a76363');
+}
+
+function scrollTop()
+{
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+}
+
+function dump(arr,level)
+{
+    var dumped_text = "";
+    if(!level) level = 0;
+    
+    //The padding given at the beginning of the line.
+    var level_padding = "";
+    for(var j=0;j<level+1;j++) level_padding += "    ";
+    
+    if(typeof(arr) == 'object') { //Array/Hashes/Objects
+     for(var item in arr) {
+      var value = arr[item];
+     
+      if(typeof(value) == 'object') { //If it is an array,
+       dumped_text += level_padding + "'" + item + "' ...\n";
+       dumped_text += dump(value,level+1);
+      } else {
+       dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+      }
+     }
+    } else { //Stings/Chars/Numbers etc.
+     dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+    }
+    return dumped_text;
+} 
---
+++ platforms/windows/www/js/login.js
@@ -0,0 +1,98 @@
+﻿$(document).ready(function () {
+    loadingOff();
+    initMain();
+
+    $("#otp-cont").hide();
+
+    $("#login-pin").on('keyup', function(e){
+        switch (e.which)
+        {
+            case 8:
+                $(this).data('val', $(this).data('val').slice(0, -1));
+            break;
+            case 13:
+                $("#login-btb").click();
+            break;
+            default:
+                var regex = new RegExp(/^[0-9]$/);
+                if (regex.exec(e.key) && $(this).data('val').length < 4)
+                {
+                    $(this).data('val', $(this).data('val') + e.key);
+                }
+        }
+        setTimeout(function(){
+            var pin = $("#login-pin");
+            var pinVal = pin.val();
+            pin.val(pinVal.replace(/./gi, '*'));
+        }, 300);
+        console.log($(this).data('val'));
+    }).mask('ZZZZ', {
+        translation: {
+            'Z': {pattern: /[0-9\*]/}
+        }
+    }).select(function(){
+        $(this).val("").data('val', '');
+    });
+
+    $("#login-btn").touch(function(){
+        scrollTop();
+        if ($("#login-id").val().length < 3)
+        {
+            notify2("Please enter a valid user ID (Email, Username, or User ID#)");
+            $("#login-id").select();
+            return false;
+        }
+        var val = new Valid($("#login-pass").val())
+        if (!val.valPass())
+        {
+            notify2("Please enter a valid password!");
+            $("#login-pass").bad();
+            return false;
+        }else{
+            $("#login-pass").ok();
+        }
+
+        var newServer = $("#login-server").val();
+        val.setVal(newServer);
+        if (val.isHostname())
+        {
+            server = 'https://'+newServer;
+        }else{
+            post_error(val.message);
+        }
+
+        var label = $(this).text();
+        var thisBtn = $(this);
+        loadingOn();
+        thisBtn.html('<i class="fas fa-cog fa-spin"></i><span> Logging in...</span>').addClass('loading').blur();
+
+        var sub = new Submit(server+'/login?action=login');
+        sub.addData('id', $("#login-id").val());
+        sub.addData('pass', $("#login-pass").val());
+        sub.addData('pin', $("#login-pin").data('val'));
+        sub.addData('otp', $("#login-otp").val());
+        sub.addData('src', 'app');
+
+        sub.submit("json", function(data){
+            thisBtn.removeClass('loading').text(label);
+            loadingOff();
+            console.log(data);
+            switch (data.status)
+            {
+                case 1:
+                    login = true;
+                    doLogin();
+                break;
+                case 2:
+                    notify2(data.message, "warn");
+                    $("#pin-cont").fadeIn(function(){
+                        $("#login-pin").select();
+                    });
+                break;
+                default:
+                    notify2(data.message, "error", false);
+                    return false;
+            }
+        });
+    });
+});
