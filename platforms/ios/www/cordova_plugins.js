aecbe9edc0de30501f9cedc503b83c7ce1f8aed2
Jay Simons <jay@cloudulus.net>
2019-02-17 19:26:44 -0600

    Icons updated, added splash

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-splash": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
