9c3add3dae2d875f0f651c655dff26d03931d15d
Jay Simons <jay@cloudulus.net>
2019-03-04 21:12:22 -0600

    dev

---
+++ www/js/functions.js
@@ -0,0 +1,15 @@
+function pad(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+function formatNum(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
